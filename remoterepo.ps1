0728176b1233052c6bc98317b5ce9725773bd570
Dylan <dylancloyd@gmail.com>
2023-08-02 17:26:54 -0400

    remote repo creation

---
+++ remoterepo.ps1
@@ -0,0 +1,31 @@
+#########################################
+#####  #####  #####  #####  #####   #####
+#########################################
+
+
+######### MODULE SECTION   ##############
+
+ipmo  powershellforgithub 
+
+ipmo microsoft.powershell.consoleguitools
+
+#########  CONFIG SECTION  ##############
+
+set-psreadlineoption -predictionviewstyle listview
+
+
+
+########  ALIAS SET SECTION   ###########
+
+set-alias -name ga -value get-alias 
+
+Set-alias -name imo -value install-module
+
+set-alias -name pg -value powershellforgithub
+
+set-alias -name gh -value get-help
+
+#########  INTREGRATION TEST ############
+
+
+
